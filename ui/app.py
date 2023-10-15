e9dc6f07cb829b0e1571293b429aaecef3da80c1
Louis Qin <qyl0509@icloud.com>
2023-10-14 19:17:58 -0700

    Add profile page.

--- ui/app.py
+++ ui/app.py
@@ -5,22 +5,18 @@ import streamlit as st
 with open("example/profile.json") as fp:
 	profile = json.load(fp)
 
+def human_readable_key(key):
+	return " ".join(key.split("_"))
+
+def display_profile():
+	for k, v in profile.items():
+		st.subheader(human_readable_key(k))
+		st.write(v)
 
 def edit_profile():
 	for k, v in profile.items():
-		st.header(" ".join(k.split('_')))
-		if isinstance(v, dict):
-			for k1, v1 in v.items():
-				st.subheader(" ".join(k1.split('_')))	
-				st.write(v1)
-		elif isinstance(v, list):
-			for v1 in v:
-				st.write(v1)
-		else:
-			st.write(v)
-
-def human_readable_key(key):
-	return " ".join(key.split('_'))
+		st.text_area(human_readable_key(k), value=v)
+	st.button("submit")
 
 def write_prev_and_curr_value(key, prev_value, diff_value):
 	if len(prev_value) > len(diff_value) + 50 or isinstance(diff_value, list):
@@ -40,21 +36,9 @@ def whats_new():
 		st.header(human_readable_key(profile_key))
 		profile_value = profile[profile_key]
 		profile_diff_value = profile_diff[profile_key]
-		if isinstance(profile_diff_value, dict):
-			rejected_sub_keys = []
-			for sub_key in profile_diff_value:
-				st.subheader(" ".join(sub_key.split('_')))
-				profile_sub_value = profile_value[sub_key]
-				profile_diff_sub_value = profile_diff_value[sub_key]
-				accepted = write_prev_and_curr_value(sub_key, profile_sub_value, profile_diff_sub_value)
-				if not accepted:
-					rejected_sub_keys.append(sub_key)
-			for sub_key in rejected_sub_keys:
-				profile_diff_value.pop(sub_key)
-		else:
-			accepted = write_prev_and_curr_value(profile_key, profile_value, profile_diff_value)
-			if not accepted:
-				rejected_keys.append(profile_key)
+		accepted = write_prev_and_curr_value(profile_key, profile_value, profile_diff_value)
+		if not accepted:
+			rejected_keys.append(profile_key)
 
 	rejected_keys += [k for k in profile_diff if profile_diff[k] == {}]
 	for profile_key in rejected_keys:
@@ -63,4 +47,17 @@ def whats_new():
 	st.button("done for the day!")
 	st.write(profile_diff)
 
-whats_new()
+
+with st.sidebar:
+	st.title("life tweet")
+	page = st.radio(
+		"",
+		["profile", "see changes", "edit profile"]
+	)
+
+if page == "profile":
+	display_profile()
+elif page == "see changes":
+	whats_new()
+else:
+	edit_profile()
